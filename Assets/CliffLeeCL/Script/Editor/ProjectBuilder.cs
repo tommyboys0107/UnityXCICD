a9a1c905ca8aba454082bdf1d1216cca8091305b
Cliff Lee CL <tommyboys0107@gmail.com>
2024-06-17 00:13:37 +0800

    [Modify] Tidy up project builder.

--- Assets/CliffLeeCL/Script/Editor/ProjectBuilder.cs
+++ Assets/CliffLeeCL/Script/Editor/ProjectBuilder.cs
@@ -16,26 +16,26 @@ namespace CliffLeeCL
         [MenuItem("Cliff Lee CL/Build project Default _F5", false, 0)]
         public static void BuildProject()
         {
-            BuildTarget target = EditorUserBuildSettings.activeBuildTarget;
-            BuildSetting buildSetting = new BuildSetting("", BuildSetting.DefineSymbolConfig.Release);
+            var target = EditorUserBuildSettings.activeBuildTarget;
+            var buildSetting = new BuildSetting("", BuildSetting.DefineSymbolConfig.Debug);
 
             // Handle command line arguments.
             if (UnityEditorInternal.InternalEditorUtility.inBatchMode)
             {
                 Dictionary<string, string> commandLineArgumentToValue = ParseCommandLineArgument();
 
-                if (commandLineArgumentToValue.ContainsKey("-outputPath"))
-                    buildSetting.outputPath = commandLineArgumentToValue["-outputPath"];
-                if (commandLineArgumentToValue.ContainsKey("-defineSymbolConfig"))
-                    buildSetting.symbolConfig = (BuildSetting.DefineSymbolConfig)Enum.Parse(typeof(BuildSetting.DefineSymbolConfig), commandLineArgumentToValue["-defineSymbolConfig"]);
+                if (commandLineArgumentToValue.TryGetValue("-outputPath", out var outputPath))
+                    buildSetting.outputPath = outputPath;
+                if (commandLineArgumentToValue.TryGetValue("-defineSymbolConfig", out var defineSymbolConfig))
+                    buildSetting.symbolConfig = (BuildSetting.DefineSymbolConfig)Enum.Parse(typeof(BuildSetting.DefineSymbolConfig), defineSymbolConfig);
             }
             BuildProject(target, buildSetting);
         }
 
-        [MenuItem("Cliff Lee CL/Build project Windowsx64 _F6", false, 1)]
+        [MenuItem("Cliff Lee CL/Build project Windows 64-bit _F6", false, 1)]
         public static void BuildProjectWindows64()
         {
-            BuildProject(BuildTarget.StandaloneWindows64, new BuildSetting("", BuildSetting.DefineSymbolConfig.Release));
+            BuildProject(BuildTarget.StandaloneWindows64, new BuildSetting("", BuildSetting.DefineSymbolConfig.Debug));
         }
 
         [MenuItem("Cliff Lee CL/Build project WebGL _F7", false, 2)]
@@ -49,11 +49,10 @@ namespace CliffLeeCL
         /// </summary>
         /// <param name="buildTarget">Target platform.</param>
         /// <param name="buildSetting">The custom build setting.</param>
-        static void BuildProject(BuildTarget buildTarget, BuildSetting buildSetting)
+        private static void BuildProject(BuildTarget buildTarget, BuildSetting buildSetting)
         {
-            BuildReport buildReport;
-            string defineSymbolBeforeBuild = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildPipeline.GetBuildTargetGroup(buildTarget));
-            BuildPlayerOptions buildPlayerOption = new BuildPlayerOptions
+            var defineSymbolBeforeBuild = PlayerSettings.GetScriptingDefineSymbolsForGroup(BuildPipeline.GetBuildTargetGroup(buildTarget));
+            var buildPlayerOption = new BuildPlayerOptions
             {
                 scenes = EditorBuildSettings.scenes.Where((s) => s.enabled).Select((s) => s.path).ToArray(),
                 locationPathName = GetBuildPath(buildTarget, buildSetting.outputPath),
@@ -62,10 +61,10 @@ namespace CliffLeeCL
             };
             PlayerSettings.SetScriptingDefineSymbolsForGroup(
                 BuildPipeline.GetBuildTargetGroup(buildTarget),
-                BuildSetting.symbolConfigToDefineSymbol[buildSetting.symbolConfig]
+                BuildSetting.ConfigToDefineSymbolDict[buildSetting.symbolConfig]
                 );
 
-            buildReport = BuildPipeline.BuildPlayer(buildPlayerOption);
+            var buildReport = BuildPipeline.BuildPlayer(buildPlayerOption);
             if (buildReport.summary.result == BuildResult.Succeeded)
             {
                 Debug.Log("[ProjectBuilder] Build Success: Time:" + buildReport.summary.totalTime + " Size:" + buildReport.summary.totalSize + " bytes");
@@ -85,15 +84,13 @@ namespace CliffLeeCL
             Debug.Log("Build project at: " + buildPlayerOption.locationPathName);
         }
 
-        static string GetBuildPath(BuildTarget buildTarget, string outputPath = "")
+        private static string GetBuildPath(BuildTarget buildTarget, string outputPath = "")
         {
-            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-            string fileName = PlayerSettings.productName + GetFileExtension(buildTarget);
-            string timeStamp = DateTime.Now.ToString("yyyyMMdd-HH-mm");
+            var desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
             string buildPath;
 
             outputPath = (outputPath == "") ? desktopPath : outputPath;
-            buildPath = Path.Combine(outputPath, PlayerSettings.productName, $"{buildTarget}_{timeStamp}", fileName);
+            buildPath = Path.Combine(outputPath, $"{PlayerSettings.productName}-{buildTarget}");
             buildPath = buildPath.Replace(@"\", @"\\");
 
             return buildPath;
@@ -102,51 +99,23 @@ namespace CliffLeeCL
         /// <summary>
         /// Parse command line argument and extract custom command line arguments.
         /// </summary>
-        /// -outputPath <pathName>: Set output path (directory) for executables.
-        /// -defineSymbolConfig <configName>: Set config for define symbol.
+        /// -outputPath: Set output path (directory) for executables.
+        /// -defineSymbolConfig: Set config for define symbol.
         /// <returns>Dictionary about custom command line arguments.</returns>
-        static Dictionary<string, string> ParseCommandLineArgument()
+        private static Dictionary<string, string> ParseCommandLineArgument()
         {
-            Dictionary<string, string> commandLineArgToValue = new Dictionary<string, string>();
-            string[] customCommandLineArg = { "-outputPath", "-defineSymbolConfig"};
-            string[] commandLineArg = Environment.GetCommandLineArgs();
+            var commandLineArgToValue = new Dictionary<string, string>();
+            var commandLineArgArray = Environment.GetCommandLineArgs();
+            var customCommandLineArgArray = new []{ "-outputPath", "-defineSymbolConfig"};
 
-            for (int i = 0; i < commandLineArg.Length; i++)
+            for (int i = 0; i < commandLineArgArray.Length; i++)
             {
-                for (int j = 0; j < customCommandLineArg.Length; j++)
-                    if (commandLineArg[i] == customCommandLineArg[j])
-                        commandLineArgToValue.Add(customCommandLineArg[j], commandLineArg[(i + 1) % commandLineArg.Length]);
+                foreach (var arg in customCommandLineArgArray)
+                    if (commandLineArgArray[i] == arg)
+                        commandLineArgToValue.Add(arg, commandLineArgArray[(i + 1) % commandLineArgArray.Length]);
             }
 
             return commandLineArgToValue;
-        }
-
-        /// <summary>
-        /// Return file extension according to build target.
-        /// </summary>
-        /// <param name="target">The build target.</param>
-        /// <returns>file extension string.</returns>
-        static string GetFileExtension(BuildTarget target)
-        {
-            switch (target)
-            {
-                case BuildTarget.StandaloneWindows:
-                case BuildTarget.StandaloneWindows64:
-                    return ".exe";
-                case BuildTarget.StandaloneOSX:
-                    return ".app";
-                case BuildTarget.StandaloneLinux64:
-                    return ".x86_64";
-                case BuildTarget.WebGL:
-                    return ".webgl";
-                case BuildTarget.Android:
-                    return EditorUserBuildSettings.buildAppBundle ? ".aab" : ".apk";
-                case BuildTarget.iOS:
-                    return ".iosversion";
-                default:
-                    Debug.LogError("No corresponding extension!");
-                    return "";
-            }
         }
     }
 
@@ -158,13 +127,14 @@ namespace CliffLeeCL
             Release
         }
 
-        public static readonly Dictionary<DefineSymbolConfig, string> symbolConfigToDefineSymbol = new Dictionary<DefineSymbolConfig, string> { 
+        public static readonly Dictionary<DefineSymbolConfig, string> ConfigToDefineSymbolDict = new()
+        { 
             [DefineSymbolConfig.Debug] = "Debug;",
             [DefineSymbolConfig.Release] = "Release;"
         };
 
-        public string outputPath = "";
-        public DefineSymbolConfig symbolConfig = DefineSymbolConfig.Debug;
+        public string outputPath;
+        public DefineSymbolConfig symbolConfig;
 
         public BuildSetting(string outputPath, DefineSymbolConfig symbolConfig)
         {
@@ -174,7 +144,7 @@ namespace CliffLeeCL
 
         public override string ToString()
         {
-            return $"{nameof(BuildSetting)}: symbolConfig={symbolConfig}, ouputPath={outputPath}";
+            return $"{nameof(BuildSetting)}: symbolConfig={symbolConfig}, outputPath={outputPath}";
         }
     }
 }
