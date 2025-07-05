c46f533e473cf36f73ff9c55458fa2fb322a40fa
Cliff Lee CL <tommyboys0107@gmail.com>
2025-07-06 01:18:54 +0800

    [Modify] Add play mode test for footman
    為專案新增了遊戲體驗（Game Feel）的程式碼模組，其中包含 Footman 角色控制器、動畫器和殭屍的程式碼。
    
    新增了 InputSystem 的參考，確保新的程式碼模組能夠使用輸入系統。
    
    同時，新增了 PlayMode 測試，用於驗證 Footman 角色在遊戲中的行為是否符合預期，例如水平和垂直移動、主要和次要攻擊等。
    
    調整了場景中的殭屍物件，並修改了專案設定，禁用了 Unity 的啟動畫面，並預載了資源。

--- Assets/PlayModeTest/FootmanTest.cs
+++ Assets/PlayModeTest/FootmanTest.cs
@@ -1,24 +1,118 @@
 using System.Collections;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
-public class FootmanTest
+public class FootmanTest : InputTestFixture
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void FootmanTestSimplePasses()
+    private Mouse mouse;
+    private Joystick joystick;
+    
+    [SetUp]
+    public override void Setup()
     {
-        // Use the Assert class to test conditions
+        base.Setup();
+        mouse = InputSystem.AddDevice<Mouse>();
+        joystick = InputSystem.AddDevice<Joystick>();
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator FootmanTestWithEnumeratorPasses()
+    public IEnumerator FootmanCanMoveHorizontally([Values(-1, 1)]int direction)
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
+        // Arrange
+        yield return SceneManager.LoadSceneAsync("GameFeel");
+        yield return new WaitForSeconds(0.5f);
+        var footmanTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        var originalPosition = footmanTransform.position;
+        
+        // Act
+        Move(joystick.stick, Vector2.right * direction, null, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        
+        // Assert
+        Assert.AreNotSame(originalPosition.x, footmanTransform.position.x);
+    }
+    
+    [UnityTest]
+    public IEnumerator FootmanCanMoveVertically([Values(-1, 1)]int direction)
+    {
+        // Arrange
+        yield return SceneManager.LoadSceneAsync("GameFeel");
+        yield return new WaitForSeconds(0.5f);
+        var footmanTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        var originalPosition = footmanTransform.position;
+        
+        // Act
+        Move(joystick.stick, Vector2.up * direction, null, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        
+        // Assert
+        Assert.AreNotSame(originalPosition.z, footmanTransform.position.z);
+    }
+
+    [UnityTest]
+    public IEnumerator FootmanCanPrimaryAttack()
+    {
+        // Arrange
+        yield return SceneManager.LoadSceneAsync("GameFeel");
+        var footmanAnimator = GameObject.FindGameObjectWithTag("Player").GetComponent<FootmanAnimator>();
+        
+        // Act
+        Press(mouse.leftButton);
+        yield return new WaitForSeconds(0.3f);
+        
+        // Assert
+        Assert.IsTrue(footmanAnimator.IsInAnimationState("LightAttack") || footmanAnimator.IsNextAnimationState("LightAttack"));
+    }
+    
+    [UnityTest]
+    public IEnumerator FootmanCanPrimaryAttackHitEnemy()
+    {
+        // Arrange
+        yield return SceneManager.LoadSceneAsync("GameFeel");
+        var zombie = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Zombie>();
+        var originalHealthPoint = zombie.healthPoint;
+        
+        // Act
+        yield return new WaitForSeconds(1.5f);
+        Press(mouse.leftButton);
+        yield return new WaitForSeconds(1.0f);
+        
+        // Assert
+        Assert.Less(zombie.healthPoint, originalHealthPoint);
+    }
+    
+    [UnityTest]
+    public IEnumerator FootmanCanSecondaryAttack()
+    {
+        // Arrange
+        yield return SceneManager.LoadSceneAsync("GameFeel");
+        var footmanAnimator = GameObject.FindGameObjectWithTag("Player").GetComponent<FootmanAnimator>();
+        
+        // Act
+        Press(mouse.rightButton);
+        yield return new WaitForSeconds(0.6f);
+        
+        // Assert
+        Assert.IsTrue(footmanAnimator.IsInAnimationState("HeavyAttack") || footmanAnimator.IsNextAnimationState("HeavyAttack"));
+    }
+    
+    [UnityTest]
+    public IEnumerator FootmanCanSecondaryAttackHitEnemy()
+    {
+        // Arrange
+        yield return SceneManager.LoadSceneAsync("GameFeel");
+        var zombie = GameObject.FindGameObjectWithTag("Enemy").GetComponent<Zombie>();
+        var originalHealthPoint = zombie.healthPoint;
+        
+        // Act
+        yield return new WaitForSeconds(1.5f);
+        Press(mouse.rightButton);
+        yield return new WaitForSeconds(1.0f);
+        
+        // Assert
+        Assert.Less(zombie.healthPoint, originalHealthPoint);
     }
 }
